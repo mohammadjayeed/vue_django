6bec9a7d451465e6d51eccd9bbdb0f5934b7a702
Mohammad Jayeed <thisisjayeed@gmail.com>
2023-08-14 13:15:16 +0600

    vue init with django

--- frontend/main.js
+++ frontend/main.js
@@ -3,4 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const el = document.getElementById('app')
+if (el){
+
+console.log(el.dataset)
+
 createApp(App).mount('#app')
+
+}
